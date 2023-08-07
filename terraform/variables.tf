06d32b382729d217be1a9c22261a82e973cfc413
Rob Edwards <rob.j.edwards@gmail.com>
2023-08-07 15:01:03 +0100

    tf variable validation test example

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,13 @@
 variable "region" {
+  description = "The region that the resource will be deployed to"
   type    = string
-  default = "us-east1"
+  default = "us-central1"
+
+  validation {
+    condition = contains(["europe-west1", "europe-west2", "us-central1"], var.region)
+    error_message = "Can only deploy to UK (europe-west2), Belgium (europe-west1) or Iowa (us-central1) due to compliance and low CO2"
+    
+  }
 }
 
 variable "instance_type" {
